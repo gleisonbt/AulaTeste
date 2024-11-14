e2d97c4c3f9ccfb58bde84f7b350fd7cac437099
gleisonbt <brittobaptista93@gmail.com>
2024-11-13 21:23:37 -0300

    new tests

--- testes/teste.py
+++ testes/teste.py
@@ -8,16 +8,19 @@ def setup_function():
     proximo_id_postagem = 1
 
 def testaCriarUsuarioValido():
+    setup_function()
     criar_usuario("Gleison")
     assert usuarios[0] == {
         'id':1, 'nome':'Gleison',
          'seguidores':[], 'seguindo':[]}
 
 def testaCriarUsuarioSemNome():
+    setup_function()
     with pytest.raises(Exception) as error:
         criar_usuario("")
 
 def testaCriarPostagemValida():
+    setup_function()
     criar_usuario("Maria")
     criar_postagem(1, "Olá Mundo")
 
@@ -25,7 +28,19 @@ def testaCriarPostagemValida():
         'id':1, 'usuario':1, 'mensagem': 'Olá Mundo'
     }
 
+def testaRetornaUsuarioPorId():
+    setup_function()
+    criar_usuario('juan')
+    usuario = encontrar_usuario_por_id(1)
+    assert usuario == {
+        'id':1,
+        'nome':'juan',
+        'seguidores':[],
+        'seguindo':[]
+    }
+
 def testaCriarPostagemUsuarioInexistente():
+    setup_function()
     with pytest.raises(IndexError) as error:
         criar_postagem(1,'relou')
 
